e259abd3c265717ff181915d22694e2a82db0062
Shannqa <116514532+Shannqa@users.noreply.github.com>
2023-02-12 21:43:19 +0100

    Add html form, add book constructor function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+let myLibrary = [];
 
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.info = function () {
+  return (
+    this.title +
+    " by " +
+    this.author +
+    ", " +
+    this.pages +
+    ", " +
+    this.read +
+    "."
+  );
+};
+
+function addBookToLibrary() {}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "read");
